e7edaf90878a1c971e62a55d6182ef916736bbe4
Ryan <jnoverly@gmail.com>
2023-02-14 01:07:59 -0500

    Work in Progress, Changing the cipher code

--- cipher.py
+++ cipher.py
@@ -1,92 +1,95 @@
-print('Caesar Cipher')
+"""THis"""
+TITLE = 'Caesar Cipher'
+print(TITLE)
 
-'''
-ENCRYPTING
-'''
+
 def cipher():
-        sentence = input('input a sentence to be encrypted : ')
+    """This function will encrypt the inputed text"""
 
-        key = int(input('input your cipher key --> '))
+    sentence = input('input a sentence to be encrypted : ')
 
-        sentence_char = []
-        sentence_ascii = []
+    key = int(input('input your cipher key --> '))
 
-        encrypted_ascii = []
+    sentence_char = []
+    sentence_ascii = []
 
-        for i in range(len(sentence)):
-                sentence_char.append(sentence[i])
-                sentence_ascii.append(ord(sentence[i]))
+    encrypted_ascii = []
 
-        encrypted = ""
+    for i in sentence:
+        sentence_char.append(i)
+        sentence_ascii.append(ord(i))
 
-        for i in range(len(sentence_ascii)):
-                if sentence_ascii[i] + key > 122:
-                        a = ((sentence_ascii[i] + key) - 122) + 97 - 1
-                        encrypted_ascii.append(a)
-                else:
-                        c = sentence_ascii[i] + key
-                        encrypted_ascii.append(c)
+    encrypted = ""
 
-        for i in encrypted_ascii:
-                if i == (32 + key):
-                        encrypted += ' '
-                else:
-                        encrypted += chr(i)
+    print(sentence_ascii)
 
-        print('Encrypted --> ',encrypted)
+    for i in sentence_ascii:
+        if i + key > 122:
+            a = ((i + key) - 122) + 97 - 1
+            encrypted_ascii.append(a)
+        else:
+            c = i + key
+            encrypted_ascii.append(c)
 
-'''
-DECRYPTING
-'''
+    for i in encrypted_ascii:
+        if i == (32 + key):
+            encrypted += ' '
+        else:
+            encrypted += chr(i)
+
+    print('Encrypted --> ', encrypted)
+
+
 def decipher():
-        sentence = input('input a sentence to be decrypted: ')
-        key = int(input('input the key of the encryption --> '))
+    """Decrypting"""
+    sentence = input('input a sentence to be decrypted: ')
+    key = int(input('input the key of the encryption --> '))
 
-        sentence_char = []
-        sentence_ascii = []
+    sentence_char = []
+    sentence_ascii = []
 
-        encrypted_ascii = []
+    encrypted_ascii = []
 
-        for i in range(len(sentence)):
-                sentence_char.append(sentence[i])
-                sentence_ascii.append(ord(sentence[i]))
+    for i in sentence:
+        sentence_char.append(i)
+        sentence_ascii.append(ord(i))
 
-        decrypted = ""
+    decrypted = ""
 
-        for i in range(len(sentence_ascii)):
-                if  sentence_ascii[i] - key < 97:
-                        a = ((sentence_ascii[i] - key) + 122) - 97 + 1
-                        encrypted_ascii.append(a)
-                else:
-                        c = sentence_ascii[i] - key
-                        encrypted_ascii.append(c)
+    for i in sentence_ascii:
+        if i - key < 97:
+            a = ((i - key) + 122) - 97 + 1
+            encrypted_ascii.append(a)
+        else:
+            c = i - key
+            encrypted_ascii.append(c)
 
-        for i in encrypted_ascii:
-                if i == (((32 - key) + 122) - 97):
-                        decrypted += ' '
-                else:
-                        decrypted += chr(i)
-        
-        print('Decrypted --> ', decrypted)
+    for i in encrypted_ascii:
+        if i == (((32 - key) + 122) - 97):
+            decrypted += ' '
+        else:
+            decrypted += chr(i)
+
+    print('Decrypted --> ', decrypted)
+
 
 def menu():
-        print('\nWhat would you like to do?\n1. Encrypting \n2. Decrypting \n3. Quit\n')
+    """This is the start menu"""
+    print('\nWhat would you like to do?\n1. Encrypting \n2. Decrypting \n3. Quit\n')
 
-        user = int(input('--> '))
+    user = int(input('--> '))
 
-        while user != 3:
-                if user == 1:
-                        cipher()
-                        menu()
-                        break
-                elif user == 2:
-                        decipher()
-                        menu()
-                        break
-                elif user == 3:
-                        break
-                else:
-                        print('This is not a valid choice')
-                        menu()
-
-menu()
+    while user != 3:
+        if user == 1:
+            cipher()
+            menu()
+            break
+        if user == 2:
+            decipher()
+            menu()
+            break
+        if user == 3:
+            break
+        else:
+            print('This is not a valid choice')
+            menu()
